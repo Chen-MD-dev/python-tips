a64e2732b518c6667a6e399f1cba23ff93be5f14
Chen-MD-dev <78338584+Chen-MD-dev@users.noreply.github.com>
2021-04-14 15:57:15 +0900

    Update parallel_proc_example.py

--- multiprocessing/parallel_proc_example.py
+++ multiprocessing/parallel_proc_example.py
@@ -6,6 +6,8 @@ multi_analy : 그냥 opencv로 영상재생한다 생각하셈
 결국 threading은 한번에 한 쓰레드만 작동하기 때문에
 정말 병렬처리를 python으로 하고 싶다면 multiprocessing이 답이다.
 각각 비동기로 프로세스를 추가하고 싶다면 apply_async를 쓰시라
+사실 이 테스트처럼 프로세스 실행할 리스트가 미리 다 정의 되어 있고
+동시에 실행시킬거라면 map_async를 쓰면 된다
 """
 import multi_analy
 import pafy
